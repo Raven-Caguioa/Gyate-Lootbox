9b2240b1830bf995a481f1db835a9287971d9e0d
Raven Caguioa <jerwincah4712@gmail.com>
2026-02-18 09:51:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6822782441-37832",
+  "appId": "1:793182912246:web:481413ea8de5b65e07f6d7",
+  "apiKey": "AIzaSyDPaQScAYCqLH_fcC84Z_ufh8o7bJtAXuM",
+  "authDomain": "studio-6822782441-37832.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "793182912246"
+};
